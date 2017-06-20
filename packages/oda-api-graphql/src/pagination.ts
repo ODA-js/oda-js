94a7f2dc884719050d302dd602a37c062e9b28bc
vedanta-krit.das <vedmalex@gmail.com>
2017-06-20 11:57:20 +0300

    fix: use default value for default page limit

--- packages/oda-api-graphql/src/pagination.ts
+++ packages/oda-api-graphql/src/pagination.ts
@@ -8,12 +8,12 @@ export default function (args, resolver = cursorToId): {
 } {
   let result: any = { limit: 10, skip: 0 };
   if (args.first || args.after) {
-    result.limit = args.first || 10;
+    result.limit = args.first || result.limit;
     if (args.after) {
       result.after = resolver(args.after);
     }
   } else if (args.last || args.before) {
-    result.limit = args.last || 10;
+    result.limit = args.last || result.limit;
     if (args.before) {
       result.before = resolver(args.before);
     }
