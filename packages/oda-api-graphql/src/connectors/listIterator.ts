452205c9392c7f6908a3cc239bf159ca673be032
vedanta-krit.das <vedmalex@gmail.com>
2017-07-28 12:15:40 +0300

    implement list iterator for paginated output from any source

---
+++ packages/oda-api-graphql/src/connectors/listIterator.ts
@@ -0,0 +1,23 @@
+require('core-js/modules/es7.symbol.async-iterator');
+// get the data
+export async function* iterate(getData: (page, limit) => Promise<any[]>, limit) {
+  let i = 0;
+  while (true) {
+    let res = await getData(i, limit);
+    if (Array.isArray(res) && res.length > 0) {
+      yield res;
+      i++;
+      if (res.length < limit) {
+        return;
+      }
+    } else {
+      break;
+    }
+  }
+}
+
+export async function* forward(getData: (step) => Promise<any[]>, limit: number): AsyncIterableIterator<any> {
+  for await (let i of iterate(getData, limit)) {
+    yield* i;
+  }
+}
