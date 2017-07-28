3c692bff3057dd93485d31be7329f4bb045e824f
vedanta-krit.das <vedmalex@gmail.com>
2017-07-28 12:19:22 +0300

    export

--- packages/oda-api-common/src/index.ts
+++ packages/oda-api-common/src/index.ts
@@ -1,6 +1,7 @@
 import { Server } from './api';
 import * as passport from './passport';
 import DbMongooseConnectionPool from './dbMongooseConnectionPool';
+import DbSequelizeConnectionPool from './dbSequelizeConnectionPool';
 import * as middleware from './middleware';
 import connectionTracer from './connectionTracer';
 
@@ -8,6 +9,7 @@ export {
   Server,
   passport,
   DbMongooseConnectionPool,
+  DbSequelizeConnectionPool,
   middleware,
   connectionTracer,
 }
