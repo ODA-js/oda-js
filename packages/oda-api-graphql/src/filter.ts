0fe3707405f1d4da24798267b3a846e389925d12
vedanta-krit.das <vedmalex@gmail.com>
2017-08-15 10:29:49 +0300

    refactor mongoid validation

--- packages/oda-api-graphql/src/filter.ts
+++ packages/oda-api-graphql/src/filter.ts
@@ -1,10 +1,28 @@
-let mongoose = require('mongoose');
 import { fromGlobalId } from 'graphql-relay';
 
-function validId(id) {
-  return mongoose.Types.ObjectId.isValid(id);
-}
+const checkForHexRegExp = new RegExp("^[0-9a-fA-F]{24}$");
 
+function validId(id) {
+  if (id == null) return false;
+
+  if (typeof id == 'number') {
+    return true;
+  }
+
+  if (typeof id == 'string') {
+    return id.length == 12 || (id.length == 24 && checkForHexRegExp.test(id));
+  }
+
+  if (id.toHexString) {
+    return id.id.length == 12 || (id.id.length == 24 && checkForHexRegExp.test(id.id));
+  }
+
+  if (id.toStirng) {
+    return id.length == 12 || (id.length == 24 && checkForHexRegExp.test(id));
+  }
+
+  return false;
+};
 
 export function getValue(value, idMap, id) {
   if (id) {
