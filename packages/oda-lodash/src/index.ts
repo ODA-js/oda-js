630f6d51619b3356f555d1b73bcb8d0876fa8146
vedanta-krit.das <vedmalex@gmail.com>
2017-09-08 17:40:50 +0300

    expose reshape

--- packages/oda-lodash/src/index.ts
+++ packages/oda-lodash/src/index.ts
@@ -1,8 +1,11 @@
 import { graphqlLodashExpress } from "./express";
 import { LodashModule } from "./lodash";
 import { runQueryLodash } from './runQuery';
+import reshape from './reshape';
+
 export {
   graphqlLodashExpress,
   LodashModule,
   runQueryLodash,
+  reshape,
 }
