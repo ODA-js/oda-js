8741ffb32b9ad90a51a91c1753ae61ca9a11fcbf
vedanta-krit.das <vedmalex@gmail.com>
2017-10-13 23:52:20 +0300

    fix: boolean

--- packages/oda-gen-common/src/types/whereBoolean.ts
+++ packages/oda-gen-common/src/types/whereBoolean.ts
@@ -5,19 +5,8 @@ export class WhereBoolean extends GQLModule {
     entry: [`
       input WhereBoolean {
         eq: Boolean
-        gt: Boolean
-        gte: Boolean
-        lt: Boolean
-        lte: Boolean
         ne: Boolean
-        in: [Boolean!]
-        nin: [Boolean!]
-        and: [WhereBoolean!]
-        or: [WhereBoolean!]
-        nor: [WhereBoolean!]
-        not: [WhereBoolean!]
         exists: Boolean
-        match: String
       }
   `],
   };
