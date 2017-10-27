64cbca28aa8bcc406302b5979583ae091293ffcd
vedanta-krit.das <vedmalex@gmail.com>
2017-10-27 16:11:45 +0300

    create uses action types

--- packages/oda-aor-rest/src/data/createSingle.ts
+++ packages/oda-aor-rest/src/data/createSingle.ts
@@ -1,6 +1,33 @@
 import * as comparator from 'comparator.js';
+import { actionType } from './../ui/consts';
 
 export default function (data, field, resource, resources) {
+  const fieldId = field + 'Id';
+  const fieldType = field + 'Type';
+  const fieldCreate = field + 'Create';
+  const fieldUnlink = field + 'Unlink';
+  switch (data[fieldType]) {
+    case actionType.USE:
+      if (data[fieldId]) {
+        return {
+          [field]: { id: data[fieldId] },
+        };
+      }
+    case actionType.CLONE:
+    case actionType.CREATE:
+      if (data[field] && typeof data[field] === 'object') {
+        let res = resources[resource].CREATE.variables({ data: data[field] }).input;
+        delete res.id;
+        return {
+          [field]: {
+            ...res,
+          },
+        };
+      }
+  }
+}
+
+/* export default function (data, field, resource, resources) {
   if (!comparator.strictEq(data[field])) {
     let res = resources[resource].CREATE.variables({ data: data[field] }).input;
     if (!comparator.looseEq({}, {
@@ -12,3 +39,4 @@ export default function (data, field, resource, resources) {
     }
   }
 }
+ */
