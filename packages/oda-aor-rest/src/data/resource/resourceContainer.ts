2c064e058c307de6bc7c64ed0afd220f9eab1ef3
vedanta-krit.das <vedmalex@gmail.com>
2017-11-21 15:10:28 +0300

    RC resource container

--- packages/oda-aor-rest/src/data/resource/resourceContainer.ts
+++ packages/oda-aor-rest/src/data/resource/resourceContainer.ts
@@ -1,16 +1,23 @@
-import { IResourceContainer, IResourceBase, IResourceOverride } from './interfaces';
+import { IResourceContainer, IResource, IResourceDefinition, IResourceOperation } from './interfaces';
 import { queries } from './consts';
+import Resource from './resource';
+export { queries }
 
 export class ResourceContainer implements IResourceContainer {
-  private resources: { [resource: string]: IResourceBase }
-  constructor() {
+  private resources: { [resource: string]: IResource }
+  constructor(options?: IResourceDefinition[] | IResourceDefinition) {
     this.resources = {};
+    if (Array.isArray(options)) {
+      options.forEach(this.register.bind(this));
+    } else if (options) {
+      this.register(options);
+    }
   }
-  public register(name: string, resource: IResourceBase) {
-    this.resources[name] = resource;
+  public register(resource: IResourceDefinition) {
+    this.resources[resource.name] = new Resource(resource);
   }
-  public override(name: string, resource: IResourceOverride) {
-    let res = this.resources[name];
+  public override(resource: IResourceDefinition) {
+    let res = this.resources[resource.name];
     res.override(resource);
   }
   public queries(resource: string, query: queries) {
