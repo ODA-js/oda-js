ff6e8b7ab8b656b516795b48817cf7021cc25acf
vedanta-krit.das <vedmalex@gmail.com>
2017-12-01 16:31:59 +0300

    Label support translation

--- packages/oda-aor-rest/src/ui/components/label.tsx
+++ packages/oda-aor-rest/src/ui/components/label.tsx
@@ -4,19 +4,20 @@ import TextFieldLabel from 'material-ui/TextField/TextFieldLabel';
 
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import * as translate from 'admin-on-rest/lib/i18n/translate';
 import styles from './styles';
 
-const Label = ({ style, text, children }, { muiTheme = getMuiTheme(lightBaseTheme) }) => {
+const Label = ({ style, text, children, translate }, { muiTheme = getMuiTheme(lightBaseTheme) }) => {
   const labelStyle = Object.assign(styles.label, {
     color: muiTheme.textField.focusColor,
   });
   return (
     <div style={styles.labelContainer} >
       <TextFieldLabel muiTheme={muiTheme} style={style || labelStyle} shrink={false}>
-        {text}
+        {translate(text)}
       </TextFieldLabel>
     </div>
   );
 }
 
-export default Label;
+export default translate(Label);
