3d635d04b7beb8d4c1f3ed4e8b5cb1dd4eabfd3c
vedanta-krit.das <vedmalex@gmail.com>
2017-12-22 20:25:36 +0300

    show errors

--- packages/oda-gen-graphql/src/generator/generator.ts
+++ packages/oda-gen-graphql/src/generator/generator.ts
@@ -1,5 +1,5 @@
 import { Factory } from 'fte.js';
-import { MetaModel } from 'oda-model';
+import { MetaModel, IValidationResult } from 'oda-model';
 import * as fs from 'fs-extra';
 import * as path from 'path';
 import * as template from '../graphql-backend-template';
@@ -20,6 +20,9 @@ import $generatePkg from './generators/package';
 import $generateModel from './generators/model';
 import templateEngine from './templateEngine';
 import initModel from './initModel';
+import { collectErrors, showLog, knownTypes, hasResult } from './validate';
+import { error } from 'util';
+import { ValidationResultType } from '../../../oda-model/dist/model/interfaces';
 
 export default (args: Generator) => {
   let {
@@ -61,92 +64,100 @@ export default (args: Generator) => {
   //mutating config...
   const { modelStore, packages, config: _config } = initModel({ pack, hooks, secureAcl, config });
 
-  config = _config;
-
-  fs.ensureDirSync(rootDir);
-
-  if (config.ts) {
-    // FIXIT:
-    // generate data layer api
-    let dataPackage = modelStore.packages.get('system');
-    let curConfig = config.packages['system'];
-    let generateData = $generateData.bind(null,
-      dataPackage, raw, rootDir, typeMapper, defaultAdapter,
-      Array.from(dataPackage.entities.values()), curConfig, 'entity');
-
-    let generatePkg = $generateDataPkg.bind(null, raw, rootDir, dataPackage, typeMapper);
-
-    generateData('data.adapter.connector', 'ts');
-    generateData('data.adapter.schema', 'ts');
-    generateData('data.types.model', 'ts');
-    // data-awarness
-    generatePkg('indexMongooseConnectors', 'connectorIndex.ts');
-    generatePkg('registerMongooseConnectors', 'registerConnectors.ts');
-  }
-
-  if (config.ts) {
-    $generateModel(raw, rootDir, { packages }, typeMapper, 'registerConnectors', 'registerConnectors.ts');
-  }
-
+  const existingTypes = knownTypes(actualTypeMapper);
   // generate per package
-  packages.forEach(pkg => {
-    let generate = $generateGraphql.bind(null, pkg, raw, rootDir, pkg.name/*role is package name*/, aclAllow, typeMapper);
-    let generatePkg = $generatePkg.bind(null, raw, rootDir, typeMapper);
-    const curConfig = config.packages[pkg.name];
-    const entities = Array.from(pkg.entities.values());
-    const mutations = Array.from(pkg.mutations.values());
+  const errors: IValidationResult[] = collectErrors(packages, existingTypes);
+  if (hasResult(errors, ValidationResultType.error)) {
+    console.error('please fix followings errors to proceed');
+    showLog(errors);
+  } else {
+    showLog(errors, [ValidationResultType.fixable, ValidationResultType.warning])
+    config = _config;
 
-    // доделать фильтрацию по названиям сущностей, пакетов и т.п.
-    // entity/connections
+    fs.ensureDirSync(rootDir);
 
-    if (!pkg.abstract) {
-      if (config.graphql) {
-        generate(entities, curConfig, 'entity', 'connections.mutations.entry', 'graphql');
-        generate(entities, curConfig, 'entity', 'connections.mutations.types', 'graphql');
-        generate(entities, curConfig, 'entity', 'connections.types', 'graphql');
-        generate(entities, curConfig, 'entity', 'mutations.entry', 'graphql');
-        generate(entities, curConfig, 'entity', 'mutations.types', 'graphql');
-        generate(entities, curConfig, 'entity', 'dataPump.queries', 'graphql');
-        generate(entities, curConfig, 'entity', 'subscriptions.entry', 'graphql');
-        generate(entities, curConfig, 'entity', 'subscriptions.types', 'graphql');
-        generate(entities, curConfig, 'entity', 'query.entry', 'graphql');
-        generate(entities, curConfig, 'entity', 'viewer.entry', 'graphql');
-        generate(entities, curConfig, 'entity', 'type.entry', 'graphql');
-        generate(entities, curConfig, 'entity', 'type.enums', 'graphql');
-        generate(mutations, curConfig, 'mutation', 'types', 'graphql');
-        generate(mutations, curConfig, 'mutation', 'entry', 'graphql');
-      }
+    if (config.ts) {
+      // FIXIT:
+      // generate data layer api
+      let dataPackage = modelStore.packages.get('system');
+      let curConfig = config.packages['system'];
+      let generateData = $generateData.bind(null,
+        dataPackage, raw, rootDir, typeMapper, defaultAdapter,
+        Array.from(dataPackage.entities.values()), curConfig, 'entity');
 
-      if (config.ts) {
-        generate(entities, curConfig, 'entity', 'connections.mutations.resolver', 'ts');
-        generate(entities, curConfig, 'entity', 'mutations.resolver', 'ts');
-        generate(entities, curConfig, 'entity', 'dataPump.config', 'ts');
-        generate(entities, curConfig, 'entity', 'subscriptions.resolver', 'ts');
-        generate(entities, curConfig, 'entity', 'query.resolver', 'ts');
-        generate(entities, curConfig, 'entity', 'viewer.resolver', 'ts');
-        generate(entities, curConfig, 'entity', 'type.resolver', 'ts');
-        generate(entities, curConfig, 'entity', 'index', 'ts');
-        generate(mutations, curConfig, 'mutation', 'resolver', 'ts');
-        generate(mutations, curConfig, 'mutation', 'index', 'ts');
+      let generatePkg = $generateDataPkg.bind(null, raw, rootDir, dataPackage, typeMapper);
 
-        generatePkg(pkg, '', 'graphqlIndex', 'index.ts');
-        generatePkg(pkg, '', 'graphqlSchema', 'schema.ts');
-
-        generatePkg(pkg, 'entity', 'typeIndex', 'index.ts');
-        generatePkg(pkg, 'entity', 'node', 'node.ts');
-        generatePkg(pkg, 'entity', 'viewer', 'viewer.ts');
-        generatePkg(pkg, 'mutation', 'mutationIndex', 'index.ts');
-      }
-      if (config.ui) {
-        generatePkg(pkg, '', 'uiIndex', 'index.js');
-        generate(entities, curConfig, 'entity', 'UI.queries', 'js');
-        generate(entities, curConfig, 'entity', 'UI.forms', 'js');
-      }
+      generateData('data.adapter.connector', 'ts');
+      generateData('data.adapter.schema', 'ts');
+      generateData('data.types.model', 'ts');
+      // data-awarness
+      generatePkg('indexMongooseConnectors', 'connectorIndex.ts');
+      generatePkg('registerMongooseConnectors', 'registerConnectors.ts');
     }
 
-    if (config.schema) {
-      generatePkg(pkg, 'schema', 'schemaPuml', 'schema.puml');
+    if (config.ts) {
+      $generateModel(raw, rootDir, { packages }, typeMapper, 'registerConnectors', 'registerConnectors.ts');
     }
-  });
 
+    // generate per package
+    packages.forEach(pkg => {
+      let generate = $generateGraphql.bind(null, pkg, raw, rootDir, pkg.name/*role is package name*/, aclAllow, typeMapper);
+      let generatePkg = $generatePkg.bind(null, raw, rootDir, typeMapper);
+      const curConfig = config.packages[pkg.name];
+      const entities = Array.from(pkg.entities.values());
+      const mutations = Array.from(pkg.mutations.values());
+
+      // доделать фильтрацию по названиям сущностей, пакетов и т.п.
+      // entity/connections
+
+      if (!pkg.abstract) {
+        if (config.graphql) {
+          generate(entities, curConfig, 'entity', 'connections.mutations.entry', 'graphql');
+          generate(entities, curConfig, 'entity', 'connections.mutations.types', 'graphql');
+          generate(entities, curConfig, 'entity', 'connections.types', 'graphql');
+          generate(entities, curConfig, 'entity', 'mutations.entry', 'graphql');
+          generate(entities, curConfig, 'entity', 'mutations.types', 'graphql');
+          generate(entities, curConfig, 'entity', 'dataPump.queries', 'graphql');
+          generate(entities, curConfig, 'entity', 'subscriptions.entry', 'graphql');
+          generate(entities, curConfig, 'entity', 'subscriptions.types', 'graphql');
+          generate(entities, curConfig, 'entity', 'query.entry', 'graphql');
+          generate(entities, curConfig, 'entity', 'viewer.entry', 'graphql');
+          generate(entities, curConfig, 'entity', 'type.entry', 'graphql');
+          generate(entities, curConfig, 'entity', 'type.enums', 'graphql');
+          generate(mutations, curConfig, 'mutation', 'types', 'graphql');
+          generate(mutations, curConfig, 'mutation', 'entry', 'graphql');
+        }
+
+        if (config.ts) {
+          generate(entities, curConfig, 'entity', 'connections.mutations.resolver', 'ts');
+          generate(entities, curConfig, 'entity', 'mutations.resolver', 'ts');
+          generate(entities, curConfig, 'entity', 'dataPump.config', 'ts');
+          generate(entities, curConfig, 'entity', 'subscriptions.resolver', 'ts');
+          generate(entities, curConfig, 'entity', 'query.resolver', 'ts');
+          generate(entities, curConfig, 'entity', 'viewer.resolver', 'ts');
+          generate(entities, curConfig, 'entity', 'type.resolver', 'ts');
+          generate(entities, curConfig, 'entity', 'index', 'ts');
+          generate(mutations, curConfig, 'mutation', 'resolver', 'ts');
+          generate(mutations, curConfig, 'mutation', 'index', 'ts');
+
+          generatePkg(pkg, '', 'graphqlIndex', 'index.ts');
+          generatePkg(pkg, '', 'graphqlSchema', 'schema.ts');
+
+          generatePkg(pkg, 'entity', 'typeIndex', 'index.ts');
+          generatePkg(pkg, 'entity', 'node', 'node.ts');
+          generatePkg(pkg, 'entity', 'viewer', 'viewer.ts');
+          generatePkg(pkg, 'mutation', 'mutationIndex', 'index.ts');
+        }
+        if (config.ui) {
+          generatePkg(pkg, '', 'uiIndex', 'index.js');
+          generate(entities, curConfig, 'entity', 'UI.queries', 'js');
+          generate(entities, curConfig, 'entity', 'UI.forms', 'js');
+        }
+      }
+
+      if (config.schema) {
+        generatePkg(pkg, 'schema', 'schemaPuml', 'schema.puml');
+      }
+    });
+  }
 };
