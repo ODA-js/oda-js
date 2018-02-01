11dbb6d77114c74f777a64c142580a80921db2a3
vedanta-krit.das <vedmalex@gmail.com>
2018-02-01 18:02:49 +0300

    pre

--- packages/oda-model/src/validation/__tests__/schema/entities/Group.ts
+++ packages/oda-model/src/validation/__tests__/schema/entities/Group.ts
@@ -1,6 +1,6 @@
-import { IEntityInit } from '../../../interfaces/IEntity';
+import { IEntityInput } from '../../../interfaces/IEntity';
 
-const entity: IEntityInit = {
+const entity: IEntityInput = {
   name: 'Group',
   description: 'Group of users that can share common ToDos',
   fields: {
