50761ed47e4e54e4dc159d4c0ad172cde7081cc9
vedanta-krit.das <vedmalex@gmail.com>
2018-02-06 15:19:49 +0300

    persistent

--- packages/oda-model/src/model/Persistent.ts
+++ packages/oda-model/src/model/Persistent.ts
@@ -4,23 +4,41 @@ import { IValidationResult } from '../interfaces/IValidationResult';
 import { IValidator } from '../interfaces/IValidator';
 
 export abstract class Persistent<TInputProps, TStoredProps> implements IUpdatable {
-  protected store: Record<Partial<TStoredProps>>;
+  protected store: Record<TStoredProps>;
   protected init: Partial<TInputProps>;
+
+  /**
+   * validate current item
+   * @param validator
+   */
   public validate(validator: IValidator): IValidationResult[] {
     return validator.check(this);
   }
+
+  /**
+   * from input to stored
+   * @param input input type
+   */
   protected transform(input: Partial<TInputProps>): Partial<TStoredProps> {
     throw new Error('not implemented');
   }
-  protected reverse(input: Partial<TStoredProps>): Partial<TInputProps> {
+
+  /**
+   * from stored to input
+   * @param stored
+   */
+  protected reverse(stored: Partial<TStoredProps>): Partial<TInputProps> {
     throw new Error('not implemented');
   }
-  public updateWith<T extends TInputProps>(obj: Partial<T> ) {
+
+  public updateWith<T extends TInputProps>(obj: Partial<T>) {
     this.store = this.store.mergeDeep(this.transform(obj));
   }
-  public toJS() {
+
+  public toJS(): Partial<TInputProps> {
     return this.reverse(this.store.toJS());
   }
+
   public clone() {
     const t = this.constructor() as Persistent<TInputProps, TStoredProps>;
     t.store = this.store;
