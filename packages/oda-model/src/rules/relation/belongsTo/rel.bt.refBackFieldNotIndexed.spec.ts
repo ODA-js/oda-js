f2466148a94f005854bcf3b022112d6c3ad2584e
vedanta-krit.das <vedmalex@gmail.com>
2018-02-28 15:45:06 +0300

    test

---
+++ packages/oda-model/src/rules/relation/belongsTo/rel.bt.refBackFieldNotIndexed.spec.ts
@@ -0,0 +1,52 @@
+import Rule from './refBackFieldNotIndexed';
+
+describe('rule', () => {
+  let rule: Rule;
+  beforeAll(() => {
+    rule = new Rule();
+  });
+
+  it('throw', () => {
+    const updateWith = jest.fn();
+    const result = rule.validate({
+      relation: {
+        ref: {
+          backField: true,
+        },
+      },
+      entity: {
+        fields: {
+          get: jest.fn(() => ({
+            indexed: false,
+            updateWith,
+          })),
+        },
+      },
+    } as any);
+    expect(updateWith).toBeCalledWith({ indexed: true });
+    expect(result).toMatchSnapshot();
+  });
+
+  it('not throw 1', () => {
+    const updateWith = jest.fn();
+    const result = rule.validate({
+      relation: {
+        ref: {
+          backField: true,
+          updateWith,
+        },
+      },
+      entity: {
+        fields: {
+          get: jest.fn(() => ({
+            indexed: true,
+            updateWith,
+          })),
+        },
+      },
+    } as any);
+    expect(updateWith).not.toBeCalled();
+    expect(result).toMatchSnapshot();
+  });
+
+});
