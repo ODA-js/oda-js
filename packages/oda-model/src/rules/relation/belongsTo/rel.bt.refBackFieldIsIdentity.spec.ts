214ff590a2dc7047c9122c151166d16527cdf176
vedanta-krit.das <vedmalex@gmail.com>
2018-02-28 13:07:52 +0300

    test

---
+++ packages/oda-model/src/rules/relation/belongsTo/rel.bt.refBackFieldIsIdentity.spec.ts
@@ -0,0 +1,60 @@
+import Rule from './refBackFieldIsIdentity';
+
+describe('rule', () => {
+  let rule: Rule;
+  beforeAll(() => {
+    rule = new Rule();
+  });
+
+  it('throw', () => {
+    const result = rule.validate({
+      relation: {
+        ref: {
+          backField: true,
+        },
+      },
+      entity: {
+        fields: {
+          get: jest.fn(() => ({
+            identity: true,
+          })),
+        },
+      },
+    } as any);
+    expect(result).toMatchSnapshot();
+  });
+
+  it('not throw 1', () => {
+    const result = rule.validate({
+      relation: {
+        ref: {
+          backField: true,
+        },
+      },
+      entity: {
+        fields: {
+          get: jest.fn(() => ({
+            identity: false,
+          })),
+        },
+      },
+    } as any);
+    expect(result).toMatchSnapshot();
+  });
+
+  it('not throw 2', () => {
+    const result = rule.validate({
+      relation: {
+        ref: {
+          backField: true,
+        },
+      },
+      entity: {
+        fields: {
+          get: jest.fn(() => null),
+        },
+      },
+    } as any);
+    expect(result).toMatchSnapshot();
+  });
+});
