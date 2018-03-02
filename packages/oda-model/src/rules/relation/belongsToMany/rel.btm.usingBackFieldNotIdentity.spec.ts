b6ce3663d9d4e4de026eb6fca1f932b699a1ad1a
vedanta-krit.das <vedmalex@gmail.com>
2018-03-02 10:13:56 +0300

    test

---
+++ packages/oda-model/src/rules/relation/belongsToMany/rel.btm.usingBackFieldNotIdentity.spec.ts
@@ -0,0 +1,54 @@
+import Rule from './usingBackFieldNotIdentity';
+
+describe('rule', () => {
+  let rule: Rule;
+  beforeAll(() => {
+    rule = new Rule();
+  });
+
+  it('throw', () => {
+    const updateWith = jest.fn();
+    const result = rule.validate({
+      relation: {
+        modelType: 'relation',
+        verb: 'BelongsToMany',
+        using: {
+          backField: true,
+        },
+      },
+      entity: {
+        fields: {
+          get: jest.fn(() => ({
+            identity: false,
+            updateWith,
+          })),
+        },
+      },
+    } as any);
+    expect(updateWith).toBeCalledWith({ identity: true });
+    expect(result).toMatchSnapshot();
+  });
+
+  it('not throw 1', () => {
+    const updateWith = jest.fn();
+    const result = rule.validate({
+      relation: {
+        modelType: 'relation',
+        verb: 'BelongsToMany',
+        using: {
+          backField: true,
+        },
+      },
+      entity: {
+        fields: {
+          get: jest.fn(() => ({
+            updateWith,
+            identity: true,
+          })),
+        },
+      },
+    } as any);
+    expect(updateWith).not.toBeCalled();
+    expect(result).toMatchSnapshot();
+  });
+});
