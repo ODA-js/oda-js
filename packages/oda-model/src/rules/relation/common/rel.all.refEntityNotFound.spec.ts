478631ebc383c5671f3a1f1c2cfacda680f7788f
vedanta-krit.das <vedmalex@gmail.com>
2018-03-05 12:58:04 +0300

    tests

---
+++ packages/oda-model/src/rules/relation/common/rel.all.refEntityNotFound.spec.ts
@@ -0,0 +1,45 @@
+import Rule from './refEntityNotFound';
+
+describe('rule', () => {
+  let rule: Rule;
+  beforeAll(() => {
+    rule = new Rule();
+  });
+
+  it('throw', () => {
+    const result = rule.validate({
+      relation: {
+        ref: {
+          entity: true,
+        },
+      },
+      package: {
+        items: {
+          get: jest.fn().mockReturnValueOnce({
+            modelType: 'notEntity',
+          }),
+        },
+      },
+    } as any);
+    expect(result).toMatchSnapshot();
+  });
+
+  it('not throw 1', () => {
+    const result = rule.validate({
+      relation: {
+        ref: {
+          entity: true,
+        },
+      },
+      package: {
+        items: {
+          get: jest.fn().mockReturnValueOnce({
+            modelType: 'entity',
+          }),
+        },
+      },
+    } as any);
+    expect(result).toMatchSnapshot();
+  });
+
+});
---
+++ packages/oda-model/src/rules/relation/common/rel.all.refFieldNotIndexed.spec.ts
@@ -0,0 +1,61 @@
+import Rule from './refFieldNotIndexed';
+
+describe('rule', () => {
+  let rule: Rule;
+  beforeAll(() => {
+    rule = new Rule();
+  });
+
+  it('throw', () => {
+    const updateWith = jest.fn();
+    const result = rule.validate({
+      relation: {
+        ref: {
+          entity: true,
+        },
+      },
+      package: {
+        items: {
+          get: jest.fn().mockReturnValueOnce({
+            modelType: 'entity',
+            fields: {
+              get: jest.fn().mockReturnValueOnce({
+                indexed: false,
+                updateWith,
+              }),
+            },
+          }),
+        },
+      },
+    } as any);
+    expect(updateWith).toBeCalledWith({indexed: true});
+    expect(result).toMatchSnapshot();
+  });
+
+  it('not throw 1', () => {
+    const updateWith = jest.fn();
+    const result = rule.validate({
+      relation: {
+        ref: {
+          entity: true,
+        },
+      },
+      package: {
+        items: {
+          get: jest.fn().mockReturnValueOnce({
+            modelType: 'entity',
+            fields: {
+              get: jest.fn().mockReturnValueOnce({
+                indexed: true,
+                updateWith,
+              }),
+            },
+          }),
+        },
+      },
+    } as any);
+    expect(updateWith).not.toBeCalled();
+    expect(result).toMatchSnapshot();
+  });
+
+});
