d8e52b5ba92904e16a6a0e23192c3c6ba47f20c2
vedanta-krit.das <vedmalex@gmail.com>
2018-03-21 16:53:00 +0300

    update

--- packages/oda-aor-rest/src/ui/components/referenceManyField.tsx
+++ packages/oda-aor-rest/src/ui/components/referenceManyField.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Component } from 'react';
-import PropTypes from 'prop-types';
+import * as PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import LinearProgress from 'material-ui/LinearProgress';
 import { crudGetManyReference as crudGetManyReferenceAction } from 'admin-on-rest/lib/actions/dataActions';
@@ -14,52 +14,6 @@ import {
   SORT_DESC,
 } from 'admin-on-rest/lib/reducer/admin/resource/list/queryReducer';
 
-/**
- * Render related records to the current one.
- *
- * You must define the fields to be passed to the iterator component as children.
- *
- * @example Display all the comments of the current post as a datagrid
- * <ReferenceManyField reference="comments" target="post_id">
- *     <Datagrid>
- *         <TextField source="id" />
- *         <TextField source="body" />
- *         <DateField source="created_at" />
- *         <EditButton />
- *     </Datagrid>
- * </ReferenceManyField>
- *
- * @example Display all the books by the current author, only the title
- * <ReferenceManyField reference="books" target="author_id">
- *     <SingleFieldList>
- *         <ChipField source="title" />
- *     </SingleFieldList>
- * </ReferenceManyField>
- *
- * By default, restricts the possible values to 25. You can extend this limit
- * by setting the `perPage` prop.
- *
- * @example
- * <ReferenceManyField perPage={10} reference="comments" target="post_id">
- *    ...
- * </ReferenceManyField>
- *
- * By default, orders the possible values by id desc. You can change this order
- * by setting the `sort` prop (an object with `field` and `order` properties).
- *
- * @example
- * <ReferenceManyField sort={{ field: 'created_at', order: 'DESC' }} reference="comments" target="post_id">
- *    ...
- * </ReferenceManyField>
- *
- * Also, you can filter the query used to populate the possible values. Use the
- * `filter` prop for that.
- *
- * @example
- * <ReferenceManyField filter={{ is_published: true }} reference="comments" target="post_id">
- *    ...
- * </ReferenceManyField>
- */
 export class ReferenceManyField extends Component {
   constructor(props) {
     super(props);
