f690a02c0d6a2782ee0629e7a34416bc725afb4d
vedanta-krit.das <vedmalex@gmail.com>
2018-09-04 18:31:18 +0300

    update syntax

--- packages/oda-gen-graphql/src/graphql-backend-template/schema/ensure.ts
+++ packages/oda-gen-graphql/src/graphql-backend-template/schema/ensure.ts
@@ -53,9 +53,13 @@ export function _mapper(
             type: typeMapper.graphql(f.type),
           }))
           .sort((a, b) => {
-            if (a.name > b.name) return 1;
-            else if (a.name < b.name) return -1;
-            else return 0;
+            if (a.name > b.name) {
+              return 1;
+            } else if (a.name < b.name) {
+              return -1;
+            } else {
+              return 0;
+            }
           });
         return {
           name: i.name,
