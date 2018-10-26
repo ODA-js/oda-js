9277c365dd323e0ac4aa9c050338f337c0f2f095
vedanta-krit.das <vedmalex@gmail.com>
2018-10-26 17:37:53 +0300

    queries is a part of package

--- packages/oda-gen-graphql/src/generator/interfaces.ts
+++ packages/oda-gen-graphql/src/generator/interfaces.ts
@@ -117,6 +117,7 @@ export type Generator = {
     | {
         name: string;
         mutations?: any[];
+        queries?: any[];
         entities?: any[];
         packages?: any[];
         enums?: any[];
